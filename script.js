94a50c6dbd2a8e052163989a18915a8330038c45
Goutham Kulall <gouthamkulall615@gmail.com>
2026-03-04 16:06:33 +0530

    added manifest.json file

--- script.js
+++ script.js
@@ -110,3 +110,11 @@ addTaskBtn.addEventListener('click', () => {
 
     modal.classList.remove('active')
 })
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js")
+      .then(() => console.log("Service Worker Registered"))
+      .catch(err => console.log("SW registration failed", err));
+  });
+}
